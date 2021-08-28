7df66c915833ff4b6351bbeca83e0840b07b5c20
sarowar hossain <42488988+sarowar75856@users.noreply.github.com>
2021-08-28 22:39:07 +0600

    Update footer.blade.php

--- resources/views/layouts/body/footer.blade.php
+++ resources/views/layouts/body/footer.blade.php
@@ -45,8 +45,7 @@ $datas=DB::table('contacts')->first();
       </div>
 
       <div class="col-lg-4 col-md-6 footer-newsletter">
-        <h4>Join Our Newsletter</h4>
-        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+        <h4>Join Our Team</h4>
         <form action="" method="post">
           <input type="email" name="email"><input type="submit" value="Subscribe">
         </form>
